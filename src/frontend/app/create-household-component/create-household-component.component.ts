e91cdd6243e841da16318bc411bb35536f69419f
Koehler, Niklas <nkoehler001@student.franciscan.edu>
2024-03-08 09:50:24 -0500

    Initial Create Household Component

---
+++ src/frontend/app/create-household-component/create-household-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-create-household-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-household-component.component.html',
+  styleUrl: './create-household-component.component.css'
+})
+export class CreateHouseholdComponentComponent {
+
+}
