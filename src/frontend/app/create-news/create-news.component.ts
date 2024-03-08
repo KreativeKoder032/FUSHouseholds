32f90fbe32c6dff00668790dabfce8eeaf626997
Suskowicz, Ethan <esuskowicz001@student.franciscan.edu>
2024-03-08 09:49:43 -0500

    add generated component
    adds component for news

---
+++ src/frontend/app/create-news/create-news.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-create-news',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-news.component.html',
+  styleUrl: './create-news.component.css'
+})
+export class CreateNewsComponent {
+
+}
