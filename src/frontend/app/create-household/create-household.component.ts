d9eee771eed714c81be56c1d575661d91b6c1e0a
Koehler, Niklas <nkoehler001@student.franciscan.edu>
2024-03-22 09:25:01 -0400

    Updated the name of the component

---
+++ src/frontend/app/create-household/create-household.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-create-household',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-household.component.html',
+  styleUrl: './create-household.component.css'
+})
+export class CreateHouseholdComponent {
+
+}
