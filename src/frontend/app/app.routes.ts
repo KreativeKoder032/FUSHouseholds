c43078b84aa1aab4e3fe299f6fa0f92ec52eedda
Koehler, Niklas <nkoehler001@student.franciscan.edu>
2024-03-22 09:51:46 -0400

    fix mistake to match origin/main

--- src/frontend/app/app.routes.ts
+++ src/frontend/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { CreateHouseholdComponent } from './create-household/create-household.component';
 
-export const routes: Routes = [
-  {
-    path: 'create-household',
-    component: CreateHouseholdComponent,
-  }
-];
+export const routes: Routes = [];
