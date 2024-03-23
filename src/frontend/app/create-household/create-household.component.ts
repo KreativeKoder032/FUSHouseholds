c66adb0101ab79fee57c790147fb30e0ff54425a
Koehler, Niklas <nkoehler001@student.franciscan.edu>
2024-03-22 23:24:15 -0400

    Add household form page

--- src/frontend/app/create-household/create-household.component.ts
+++ src/frontend/app/create-household/create-household.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'ng-create-household',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-household.component.html',
   styleUrl: './create-household.component.css'
 })
 export class CreateHouseholdComponent {
-
+  name: string = '';
+  //TODO: active
+  //TODO: logo
+  year: string = '';
+  location: string = '';
+  verse: string = '';
+  //TODO: saints
+  //TODO: pillars
+  //TODO: commitments
+  covenant: string = '';
+  big_little_title: string = '';
+  sibling_household: string = '';
+  description: string = '';
+  //TODO: photos
+  //TODO: aesthetics
+  //TODO: news
 }
