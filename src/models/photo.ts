59371cda33a67a087212835eb8310ef779482af3
Christopher Barreras <cbarreras002@student.franciscan.edu>
2024-03-23 14:47:11 -0400

    inital photo model

---
+++ src/models/photo.ts
@@ -0,0 +1,28 @@
+import { News } from "./news";
+import { Household } from "./household";
+
+//LATER
+//news
+//saint
+//household
+
+export interface Photo {
+  // Unique photo id
+  id : number,
+  // Photo name
+  name : string,
+  // What type of photo {Logo, Household, Member, News, Saint, Household_Normal}
+  type : string,
+  // String make up of the photo
+  data : string,
+  // Description
+  alternite : string,
+  // Currently being used
+  active : boolean,
+  // Image of a certian saint
+    //saint : Saint,
+  // Household news
+  news_id? : News,
+  // Ties photo to a household
+  household_id? : Household,
+}
