106781f85ade10896fbe566ebe53907594b065e0
Koehler, Niklas <nkoehler001@student.franciscan.edu>
2024-04-08 10:18:33 -0400

    Recreate Household List Component

--- src/frontend/app/list-households/list-households.component.ts
+++ src/frontend/app/list-households/list-households.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Household } from '../../../models/household';
+import { HouseholdService } from '../services/household.service';
 
 @Component({
   selector: 'ng-list-households',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './list-households.component.html',
   styleUrl: './list-households.component.css'
 })
-export class ListHouseholdsComponent {
+export class ListHouseholdsComponent implements OnInit {
+  constructor(private householdService: HouseholdService) {}
+  selectedHousehold?: Household;
+  households: Household[] = [];
+
+  ngOnInit(): void {
+    this.getHouseholds();
+  }
+
+  getHouseholds(): void {
+    this.householdService.getHouseholds().subscribe(households => this.households = households);
+  }
+
+  /*households: Household[] = [
+    {
+      id: 1,
+      name: "Conquer Through Love",
+      year: 1991,
+      location: "1st Floor Louis Hall",
+      verse: "We believe that love...",
+      covenant: "etc",
+      big_little_title: "Love Bear/Love Cub",
+    }
+  ]*/
 
 }
