93766d846e105882dbf6b0b6ed2bbeeef2e500ea
Koehler, Niklas <nkoehler001@student.franciscan.edu>
2024-04-08 10:15:03 -0400

    New List Households Component

---
+++ src/frontend/app/list-households/list-households.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-list-households',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-households.component.html',
+  styleUrl: './list-households.component.css'
+})
+export class ListHouseholdsComponent {
+
+}
