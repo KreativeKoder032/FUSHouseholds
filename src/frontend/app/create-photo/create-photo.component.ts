73fa8613d9f927d9cfa7f24a693991a8e32fcefd
Christopher Barreras <cbarreras002@student.franciscan.edu>
2024-04-09 22:26:55 -0400

    initial commit

--- src/frontend/app/create-photo/create-photo.component.ts
+++ src/frontend/app/create-photo/create-photo.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Photo } from '../../../models/photo';
+import { PhotoService } from '../services/photo.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ng-create-photo',
@@ -8,5 +11,28 @@ import { Component } from '@angular/core';
   styleUrl: './create-photo.component.css'
 })
 export class CreatePhotoComponent {
+  
+  constructor(private photoService: PhotoService, private router: Router) {}
 
+  name: string = "";
+  type: string = "";
+  active: boolean = false;
+
+  save(): void {
+    const toSave: Photo = {
+      name: this.name,
+      type: this.type,
+      //TODO
+      id: 0,
+      data: '',
+      alternate: '',
+      //TODO
+      active: this.active,
+    }
+    
+    this.photoService.createPhoto(toSave).subscribe(photo => {
+      console.log('Saved ',photo,', returning home.');
+      this.router.navigate(['/']);
+    })
+  }
 }
