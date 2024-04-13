8f0db3c46c7ec34595116f9db8f71876eac541ef
Suskowicz, Ethan <esuskowicz001@student.franciscan.edu>
2024-04-13 11:13:18 -0400

    add code for component
    adds code for list-news.component.ts but there are currently some problems with it

--- src/frontend/app/list-news/list-news.component.ts
+++ src/frontend/app/list-news/list-news.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { News } from '../../../models/news';
+import { NewsService } from '../services/news.service';
+
 
 @Component({
   selector: 'ng-list-news',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './list-news.component.html',
   styleUrl: './list-news.component.css'
 })
-export class ListNewsComponent {
+export class ListNewsComponent implements OnInit {
+  constructor(private newsService: NewsService) {}
+  selectedNews?: News;
+  newsli: News[] = [];
 
+  ngOnInit(): void {
+    this.getNewsli();
+  }
+
+  getNewsli(): void {
+    this.newsService.getNewsli().subscribe(newsli => this.newsli = newsli);
+  }
 }
