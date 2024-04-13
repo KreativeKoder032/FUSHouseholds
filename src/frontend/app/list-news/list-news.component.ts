59ab28b787f771a2af25ff7bd94b6958395b348a
Suskowicz, Ethan <esuskowicz001@student.franciscan.edu>
2024-04-13 10:35:59 -0400

    Create components
    Creates components and finishes html

---
+++ src/frontend/app/list-news/list-news.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-list-news',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-news.component.html',
+  styleUrl: './list-news.component.css'
+})
+export class ListNewsComponent {
+
+}
