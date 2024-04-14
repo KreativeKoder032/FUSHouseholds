50a492ae7382d0d7b11a6e0cec9cdd1f81e757c0
Koehler, Niklas <nkoehler001@student.franciscan.edu>
2024-04-14 15:09:09 -0400

    Updated Testing files to Accomodate Addition of HTTP calls

--- src/frontend/app/create-household/create-household.component.spec.ts
+++ src/frontend/app/create-household/create-household.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CreateHouseholdComponent } from './create-household.component';
 
 describe('CreateHouseholdComponent', () => {
@@ -8,7 +8,7 @@ describe('CreateHouseholdComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CreateHouseholdComponent]
+      imports: [CreateHouseholdComponent, HttpClientTestingModule ]
     })
     .compileComponents();
     
