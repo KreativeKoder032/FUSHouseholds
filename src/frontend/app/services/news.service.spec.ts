5400ab3afa9f804c3c40c8e2bc88cbe5c58bbfb2
Suskowicz, Ethan <esuskowicz001@student.franciscan.edu>
2024-04-22 09:16:48 -0400

    fix http route
    The imports was in the wrong place for the news service spec file

--- src/frontend/app/services/news.service.spec.ts
+++ src/frontend/app/services/news.service.spec.ts
@@ -1,13 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NewsService } from './news.service';
+import { CreateNewsComponent } from '../create-news/create-news.component';
 
 describe('NewsService', () => {
   let service: NewsService;
 
   beforeEach(() => {
-    imports: [ NewsService, HttpClientTestingModule ]
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ CreateNewsComponent, HttpClientTestingModule ]
+    });
     service = TestBed.inject(NewsService);
   });
 
