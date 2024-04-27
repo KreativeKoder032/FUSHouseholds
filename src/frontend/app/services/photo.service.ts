2167a4bc884166b51870994cc4def7b1f7eeb04d
Christopher Barreras <cbarreras002@student.franciscan.edu>
2024-04-27 16:47:25 -0400

    added photo list function

--- src/frontend/app/services/photo.service.ts
+++ src/frontend/app/services/photo.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Photo } from '../../../models/photo';
 import { Observable } from 'rxjs';
-import { HttpHeaders } from '@angular/common/http';
+//import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +13,9 @@ export class PhotoService {
 
   createPhoto(photo: Photo): Observable<Photo> {
     //alert (photo.data);
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json' // Set Content-Type header
-    }); 
+    // const headers = new HttpHeaders({
+    //   'Content-Type': 'application/json' // Set Content-Type header
+    // }); 
     return this.http.post<Photo>("/api/photos/create", photo);
   }
   getPhotos(): Observable<Photo[]> {
